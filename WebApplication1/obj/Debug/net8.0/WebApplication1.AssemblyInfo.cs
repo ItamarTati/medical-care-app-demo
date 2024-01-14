f051aa3fa0e2d71f04bc293ba8ac182c6425ea38
ItamarTati <itamar.tati1994@gmail.com>
2024-01-14 12:10:13 +0000

    added frontend

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8b3e4d80e3d2262e4489c8d84ddd068c9c6645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70795780166e505909b69939e4ff95561d2342e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
