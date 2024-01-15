f118aaadef04cc5255aac55d6ef25818b4702e75
ItamarTati <itamar.tati1994@gmail.com>
2024-01-15 19:06:07 +0000

    added email and adjusted frontend

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,23 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly EmailService _emailService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, EmailService emailService)
     {
         _logger = logger;
+        _emailService = emailService;
     }
 
     public IActionResult Index()
     {
         return View();
     }
-
+                                    
     public IActionResult Privacy()
     {
         return View();
@@ -27,5 +30,18 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    [Route("api/send-email")]
+    public IActionResult SendEmail([FromBody] EmailModel emailModel)
+    {
+        string body = @"<p>Thank you for trying my demo!</p>
+                        <p>Please feel free to contact me on LinkedIn or at the provided phone number.</p>
+                        <p><strong>LinkedIn:</strong> <a href='[Your LinkedIn Profile URL]' target='_blank'>[Your LinkedIn Profile]</a></p>
+                        <p><strong>Phone:</strong> +447568488047</p>";
+        _emailService.SendEmail(emailModel.Recipient, "Thank You Trying My Demo", body);
+
+        return Ok(new { Message = "Email sent successfully" });
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70795780166e505909b69939e4ff95561d2342e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f051aa3fa0e2d71f04bc293ba8ac182c6425ea38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
